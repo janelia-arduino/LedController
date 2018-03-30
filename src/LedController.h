8b6cfb15bd89816051413bd8d8379e8ba670972d
Peter Polidoro <peterpolidoro@gmail.com>
2018-03-30 14:11:42 -0400

    Removed unnecessary header includes.

--- src/LedController.h
+++ src/LedController.h
@@ -7,15 +7,10 @@
 // ----------------------------------------------------------------------------
 #ifndef LED_CONTROLLER_H
 #define LED_CONTROLLER_H
-#include <Flash.h>
-#include <EEPROM.h>
-#include <Streaming.h>
 #include <ArduinoJson.h>
-#include <JsonSanitizer.h>
 #include <JsonStream.h>
 #include <Array.h>
 #include <Vector.h>
-#include <MemoryFree.h>
 #include <ConstantVariable.h>
 #include <SavedVariable.h>
 #include <Functor.h>
